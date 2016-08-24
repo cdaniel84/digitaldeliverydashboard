c041dd9003113965b4545aebd844de5ef5aa98a4
Omar Ahmadi <odahmadi15@gmail.com>
2016-08-24 15:42:36 -0400

    v.1.3.1

--- js/charts.js
+++ js/charts.js
@@ -1,9 +1,7 @@
 google.load('visualization', '1', {
-    'packages': ['corechart']
+    'packages': ['corechart','treemap', 'line']
 });
-google.load('visualization', '1', {
-    'packages': ['treemap']
-});
+
 
 
 google.setOnLoadCallback(drawChart);
@@ -46,7 +44,7 @@ function drawChart() {
             // Create the data table.
           var data2 = google.visualization.arrayToDataTable([
             ['Status', 'Value'],
-            ['Submitted',     10],
+            ['Sumbitted',     10],
             ['In Review',      6],
   //          ['Descoped',  5],
           ]);
@@ -56,7 +54,7 @@ function drawChart() {
             backgroundColor: '#F2F2F2',
             title: 'WileyPLUS',
             titlePosition: 'none',
-            pieHole: 0.4,
+            pieHole: 0.5,
             'chartArea': {'width': '100%', 'height': '80%'},
             pieSliceTextStyle: {color: 'black', fontName: 'Helvetica', fontSize: 20, bold: 'true'},
             pieSliceText: 'value',
@@ -67,8 +65,8 @@ function drawChart() {
             2: { color: '#CC3300'}}
           }; 
 
-            var chart2 = new google.visualization.PieChart(document.getElementById('donutchart'));
-            chart2.draw(data2, options2);   
+            var ideas = new google.visualization.PieChart(document.getElementById('ideas'));
+            ideas.draw(data2, options2);   
   
   
               // Create the data tables for project updates
@@ -95,8 +93,8 @@ function drawChart() {
             2: { color: 'darkgrey'}}
           }; 
 
-            var chart3 = new google.visualization.PieChart(document.getElementById('donutchart2'));
-            chart3.draw(data3, options3);
+            var dummies = new google.visualization.PieChart(document.getElementById('dummies'));
+            dummies.draw(data3, options3);
   
                 // Create the data table.
           var data4 = google.visualization.arrayToDataTable([
@@ -122,8 +120,8 @@ function drawChart() {
             2: { color: '#CC3300'}}
           }; 
 
-            var chart4 = new google.visualization.PieChart(document.getElementById('donutchart3'));
-            chart4.draw(data4, options4);
+            var wel = new google.visualization.PieChart(document.getElementById('wel'));
+            wel.draw(data4, options4);
   
                 // Create the data table.
           var data5 = google.visualization.arrayToDataTable([
@@ -149,8 +147,8 @@ function drawChart() {
             2: { color: '#CC3300'}}
           }; 
 
-            var chart5 = new google.visualization.PieChart(document.getElementById('donutchart4'));
-            chart5.draw(data5, options5);
+            var wileyplus = new google.visualization.PieChart(document.getElementById('wileyplus'));
+            wileyplus.draw(data5, options5);
   
                 // Create the data table.
           var data6 = google.visualization.arrayToDataTable([
@@ -176,7 +174,177 @@ function drawChart() {
             2: { color: '#CC3300'}}
           }; 
 
-            var chart6 = new google.visualization.PieChart(document.getElementById('donutchart5'));
-            chart6.draw(data6, options6);
-        
+            var wpls = new google.visualization.PieChart(document.getElementById('wpls'));
+            wpls.draw(data6, options6);
+
+  //create first line chart
+      var data7 = new google.visualization.DataTable();
+      data7.addColumn('number', 'X');
+      data7.addColumn('number', 'year 2017');
+      data7.addColumn('number', 'year 2016');
+
+      data7.addRows([
+        [0,  100, 210],
+        [.5, 150, 185],
+        [1,  200, 160],
+        [1.5,  250,   130],
+        [2,  210, 210],
+        [2.5,  180, 300],
+        [3,   180, 400],
+        [3.5,   , 400],
+        [4,  , 400],
+      ]);
+
+      var options7 = {
+       colors:['#66CC66','#3399CC'],      
+        backgroundColor: '#F2F2F2',
+        legend: {position: 'none'},
+        //chartArea:{left:20,top:0,width:'50%',height:'75%'},
+        hAxis: {
+          ticks: [{v:1, f:'Q1'}, {v:2, f:'Q2'}, {v:3, f:'Q3'}, {v:4, f:'Q4'}],  
+          title: '',
+          gridlines: {color: 'none'},
+          baselineColor: '#3399CC'
+        },
+        vAxis: {
+          ticks: [0,100,200,300,400, 500],           
+          title: '',
+          gridlines: {color: 'none'},
+          baselineColor: '#3399CC'
+        },
+        lineWidth: 5
+
+      };
+
+      var line = new google.visualization.LineChart(document.getElementById('line'));
+      line.draw(data7, options7);
+  
+  
+ //create 2nd line chart
+        var data8 = new google.visualization.DataTable();
+      data8.addColumn('number', 'X');
+      data8.addColumn('number', 'year 2017');
+      data8.addColumn('number', 'year 2016');
+
+      data8.addRows([
+        [0,  100, 210],
+        [.5, 150, 185],
+        [1,  200, 160],
+        [1.5,  250,   130],
+        [2,  210, 210],
+        [2.5,  180, 300],
+        [3,   180, 400],
+        [3.5,   , 400],
+        [4,  , 400],
+      ]);
+
+      var options8 = {
+       colors:['#66CC66','#3399CC'],      
+        backgroundColor: '#F2F2F2',
+        legend: {position: 'none'},
+        //chartArea:{left:20,top:0,width:'50%',height:'75%'},
+        hAxis: {
+          ticks: [{v:1, f:'Q1'}, {v:2, f:'Q2'}, {v:3, f:'Q3'}, {v:4, f:'Q4'}],  
+          title: '',
+          gridlines: {color: 'none'},
+          baselineColor: '#3399CC'
+        },
+        vAxis: {
+          ticks: [0,100,200,300,400, 500],           
+          title: '',
+          gridlines: {color: 'none'},
+          baselineColor: '#3399CC'
+        },
+        lineWidth: 5
+
+      };
+
+      var line2 = new google.visualization.LineChart(document.getElementById('line2'));
+      line2.draw(data8, options8);
+  
+  
+  //create 3rd line chart
+        var data9 = new google.visualization.DataTable();
+      data9.addColumn('number', 'X');
+      data9.addColumn('number', 'year 2017');
+      data9.addColumn('number', 'year 2016');
+
+      data9.addRows([
+        [0,  100, 210],
+        [.5, 150, 185],
+        [1,  200, 160],
+        [1.5,  250,   130],
+        [2,  210, 210],
+        [2.5,  180, 300],
+        [3,   180, 400],
+        [3.5,   , 400],
+        [4,  , 400],
+      ]);
+
+      var options9 = {
+       colors:['#66CC66','#3399CC'],      
+        backgroundColor: '#F2F2F2',
+        legend: {position: 'none'},
+        //chartArea:{left:20,top:0,width:'50%',height:'75%'},
+        hAxis: {
+          ticks: [{v:1, f:'Q1'}, {v:2, f:'Q2'}, {v:3, f:'Q3'}, {v:4, f:'Q4'}],  
+          title: '',
+          gridlines: {color: 'none'},
+          baselineColor: '#3399CC'
+        },
+        vAxis: {
+          ticks: [0,100,200,300,400, 500],           
+          title: '',
+          gridlines: {color: 'none'},
+          baselineColor: '#3399CC'
+        },
+        lineWidth: 5
+
+      };
+
+      var line3 = new google.visualization.LineChart(document.getElementById('line3'));
+      line3.draw(data9, options9);
+  
+  
+  //create 4th chart
+        var data10 = new google.visualization.DataTable();
+      data10.addColumn('number', 'X');
+      data10.addColumn('number', 'year 2017');
+      data10.addColumn('number', 'year 2016');
+
+      data10.addRows([
+        [0,  100, 210],
+        [.5, 150, 185],
+        [1,  200, 160],
+        [1.5,  250,   130],
+        [2,  210, 210],
+        [2.5,  180, 300],
+        [3,   180, 400],
+        [3.5,   , 400],
+        [4,  , 400],
+      ]);
+
+      var options10 = {
+       colors:['#66CC66','#3399CC'],      
+        backgroundColor: '#F2F2F2',
+        legend: {position: 'bottom'},
+        //chartArea:{left:20,top:0,width:'50%',height:'75%'},
+        hAxis: {
+          ticks: [{v:1, f:'Q1'}, {v:2, f:'Q2'}, {v:3, f:'Q3'}, {v:4, f:'Q4'}],  
+          title: '',
+          gridlines: {color: 'none'},
+          baselineColor: '#3399CC'
+        },
+        vAxis: {
+          ticks: [0,100,200,300,400, 500],           
+          title: '',
+          gridlines: {color: 'none'},
+          baselineColor: '#3399CC'
+        },
+        lineWidth: 5
+
+      };
+
+      var line4 = new google.visualization.LineChart(document.getElementById('line4'));
+      line4.draw(data10, options10);
 }
